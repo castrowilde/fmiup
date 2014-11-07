18727cd609dd3c210e6d641f5e64b5c3937f1db1
diedonetw <zah1@abv.bg>
2014-11-07 08:52:02 +0200

    Tasks 30.10.2014

--- 31.10.2014/task2age.cpp
+++ 31.10.2014/task2age.cpp
@@ -5,6 +5,6 @@ using namespace std;
 int main()
 {
   int age = 19;
-  cout << "Hi, I'm Hristiyan and I'm "<< age << " years old!";
+  cout << "Hi, I'm Hristiyan and I'm "<< age << " years old!" << endl;
   return 0;
 }
