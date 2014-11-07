9257d223356b27e95c98f7eee28654bfc709d4a0
diedonetw <zah1@abv.bg>
2014-11-07 09:01:44 +0200

    Tasks 7.11.2014

---
+++ 7.11.2014/task8triangles.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int n;
+    char c;
+    cout << "Enter n:" << endl;
+    cin >> n;
+    cout << "Enter symbol:" << endl;
+    cin >> c;
+    for(int i=0;i<n;i++){
+        cout << c;}
+    }
